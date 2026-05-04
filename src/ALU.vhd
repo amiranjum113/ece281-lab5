9376cab2bf79316f5c4149fac9f5cefd346ac563
DINOREX <225062012+amiranjum113@users.noreply.github.com>
2026-05-04 14:32:24 -0600

    Working commit

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -20,16 +20,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ALU is
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
@@ -40,53 +31,62 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-
 begin
-    process (i_A,i_B,i_op)
+    process (i_A, i_B, i_op)
         variable a_uns, b_uns : unsigned(8 downto 0);
         variable res_uns : unsigned (8 downto 0);
         variable res_8: std_logic_vector(7 downto 0);
         variable N, Z, C, V: std_logic;
-    
     begin
+        -- Initialize variables
         res_uns := (others => '0');
+        res_8 := (others => '0'); -- Ensure res_8 is cleared
         V := '0';
-        Z := '0';
-        a_uns := unsigned ('0' & i_A);--adding 0 to start to make i_A 9 bit, 9th bit tracks carry
+        a_uns := unsigned ('0' & i_A);
         b_uns := unsigned ('0' & i_B);
     
         case i_op is
-            when "000" => --ADD
+            when "000" => -- ADD
                 res_uns := a_uns + b_uns;
-                --checking overflow(pos+pos=neg)or(neg+neg=pos)
-                V := (i_A(7) and i_B(7) and not res_uns(7)) or (not(i_A(7)) and not(i_B(7)) and res_uns(7));
+                res_8 := std_logic_vector(res_uns(7 downto 0)); -- Assign here
+                V := (i_A(7) and i_B(7) and not res_8(7)) or (not(i_A(7)) and not(i_B(7)) and res_8(7));
              
-            when "001" => --SUB
+            when "001" => -- SUB
                 res_uns := a_uns - b_uns;
-                --checking overflow (pos-neg=neg)or(neg-pos=pos)
-                V := (i_A(7) xor i_B(7)) and (i_A(7) xor res_8(7));
-            when "010" => --AND
+                res_8 := std_logic_vector(res_uns(7 downto 0)); -- Assign here
+                V := (i_A(7) and not(i_B(7)) and not res_8(7)) or (not(i_A(7)) and (i_B(7)) and res_8(7));
+
+            when "010" => -- AND
                 res_8 := i_A and i_B;
                 res_uns := unsigned('0' & res_8);
-            when "011" => --OR
+
+            when "011" => -- OR
                 res_8 := i_A or i_B;
-                res_uns := unsigned('0' & res_8);                             
+                res_uns := unsigned('0' & res_8);                           
+
             when others =>
+                res_8 := (others => '0');
                 res_uns := (others => '0');
         end case;
-        res_8 := std_logic_vector(res_uns(7 downto 0));
-        --extractong flags:
-        C := res_uns (8);
+
+        -- FLAG EXTRACTION
+        -- This is the only logic change needed for the testbench
+        if i_op = "001" then 
+            C := not res_uns(8); -- Not-Borrow for SUB
+        else
+            C := res_uns(8);     -- Normal Carry for others
+        end if;
+
         N := res_8(7);
         
-        if res_8 = x"00" then --all 8 bits of result are zero
-            Z:='1';
+        if res_8 = x"00" then 
+            Z := '1';
         else
-            Z:='0';
+            Z := '0';
         end if;
-        --finally
+
+        -- FINAL ASSIGNMENTS
         o_result <= res_8;
         o_flags <= N & Z & C & V;
     end process; 
 end Behavioral;
-
